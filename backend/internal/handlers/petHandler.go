e44a10485420b728ceb7eccb9e51a9f600d61922
Malika <dusmagambetovaaa@gmail.com>
2025-02-09 21:45:47 +0500

    feat: add imageURL to the pet model

--- backend/internal/handlers/petHandler.go
+++ backend/internal/handlers/petHandler.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"shelter/internal/models"
@@ -15,13 +17,39 @@ func (h *Handler) CreatePet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var pet models.Pet
-	if err := json.NewDecoder(r.Body).Decode(&pet); err != nil {
-		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+	r.ParseMultipartForm(5 << 20)
+
+	name := r.FormValue("name")
+	description := r.FormValue("description")
+
+	file, handler, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Invalid image file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	filePath := "uploads/" + handler.Filename
+	outFile, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, "Failed to save image", http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	if _, err := io.Copy(outFile, file); err != nil {
+		http.Error(w, "Failed to save image", http.StatusInternalServerError)
 		return
 	}
 
+	pet := models.Pet{
+		Name:        name,
+		Description: description,
+		ImageURL:    filePath,
+	}
+
 	if err := h.service.CreatePet(pet); err != nil {
+		log.Fatal(err)
 		http.Error(w, "Failed to create pet", http.StatusInternalServerError)
 		return
 	}
@@ -31,7 +59,7 @@ func (h *Handler) CreatePet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) UpdatePetDescription(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPut {
+	if r.Method != http.MethodPatch {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
@@ -51,6 +79,35 @@ func (h *Handler) UpdatePetDescription(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := h.service.UpdatePetDescription(petID, request.Description); err != nil {
+		http.Error(w, "Failed to update pet description", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Pet description updated successfully"))
+}
+
+func (h *Handler) UpdateImageURL(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPatch {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	petID, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.Error(w, "Invalid pet ID", http.StatusBadRequest)
+		return
+	}
+
+	var request struct {
+		ImageURL string `json:"image_url"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.service.UpdatePetDescription(petID, request.ImageURL); err != nil {
 		http.Error(w, "Failed to update pet description", http.StatusInternalServerError)
 		return
 	}
